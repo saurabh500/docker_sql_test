2b797655a68d2a2040076b17ac40d220a07976bd
Saurabh Singh <sausing@microsoft.com>
2018-01-04 23:58:42 -0800

    Adding northwind and pubs db

--- WindowsExpress/buildDocker.cmd
+++ WindowsExpress/buildDocker.cmd
@@ -1,4 +1,4 @@
 docker container rm sqltest
 docker rmi -f sqlsaurabh
 docker build . -t sqlsaurabh
-docker run -e SA_PASSWORD=HappyPass!23 -e ACCEPT_EULA=Y -p 1403:1433 -d sqlsaurabh
+docker run -e SA_PASSWORD=<Strong_pass> -e ACCEPT_EULA=Y -p 1403:1433 -d sqlsaurabh
--- WindowsExpress/restore.sql
+++ WindowsExpress/restore.sql
@@ -1,3 +1,15 @@
 RESTORE DATABASE AdventureWorks2017 FROM DISK = "C:\AdventureWorks2017.bak" 
 WITH MOVE "AdventureWorks2017" TO "C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\AdventureWorks2017.mdf", 
 MOVE "AdventureWorks2017_log" TO "C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\AdventureWorks2017_log.ldf"
+
+
+RESTORE DATABASE Northwind FROM DISK = "C:\Northwind.bak" 
+WITH MOVE "Northwind" TO "C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\northwnd.mdf", 
+MOVE "Northwind_log" TO "C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\northwnd.ldf"
+
+
+RESTORE DATABASE pubs FROM DISK = "C:\pubs.bak" 
+WITH MOVE "pubs" TO "C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\pubs.mdf", 
+MOVE "pubs_log" TO "C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\pubs_log.ldf"
+
+
